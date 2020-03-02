6ce300df1a5cb9e1118214b00f91c2890f4034f6
marwinburesch <marwin@posteo.de>
2020-03-02 16:02:44 +0100

    Add and configure clean-webpack, html-webpack, webpack-cli, webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Pokedex v2.0'
+    })
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+    contentBase: './dist',
+    port: 9000
   }
 };
