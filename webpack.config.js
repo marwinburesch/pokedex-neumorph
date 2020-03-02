919a61d00c1c7fffa6104594eebdeffde7eda196
marwinburesch <marwin@posteo.de>
2020-03-02 16:16:37 +0100

    Configure SASS loader

--- webpack.config.js
+++ webpack.config.js
@@ -17,5 +17,20 @@ module.exports = {
   devServer: {
     contentBase: './dist',
     port: 9000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader'
+        ]
+      }
+    ]
   }
 };
