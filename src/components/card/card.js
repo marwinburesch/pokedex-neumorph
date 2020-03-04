c71b97718261ffa7332b83cb755f1be1095ad405
marwinburesch <marwin@posteo.de>
2020-03-04 11:42:00 +0100

    Add comments on info/design sources

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,6 +2,12 @@ import './card.scss';
 import { createCardHeader } from './cardHeader';
 import { createElement } from '../../lib/dom';
 
+// design: https://www.figma.com/file/fnDoJA2SQy2IK2Z5gEgGoB/pokedexv2?node-id=0%3A1
+// meta, weaknesses, evolution: https://pokeapi.co/api/v2/pokemon/metapod/
+// basestats, type: https://github.com/marwinburesch/pokemon.json/blob/master/db.json e.g. json-server
+// images: https://github.com/marwinburesch/pokemon.json/blob/master/images/${POKEMONID}.png?raw=true
+// flavortext: https://pokeapi.co/api/v2/pokemon-species/11/
+
 const pokemon = {
   id: 11,
   name: 'Metapod',
