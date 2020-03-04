9bcd2f0ecf66e537c7a96322b3f2e99e0acad57d
marwinburesch <marwin@posteo.de>
2020-03-04 11:35:29 +0100

    Modify card to read data from object

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -2,8 +2,15 @@ import './card.scss';
 import { createCardHeader } from './cardHeader';
 import { createElement } from '../../lib/dom';
 
+const pokemon = {
+  id: 11,
+  name: 'Metapod',
+  flavortext:
+    'Its shell is filled with a thick liquid. All of the\ncells throughout its body are being rebuilt in\npreparation for evolution.'
+};
+
 export function createCard() {
-  const pokemon = createElement('article', { className: 'card' });
-  pokemon.appendChild(createCardHeader(11, 'Metapod'));
-  return pokemon;
+  const card = createElement('article', { className: 'card' });
+  card.appendChild(createCardHeader(pokemon.id, pokemon.name));
+  return card;
 }
